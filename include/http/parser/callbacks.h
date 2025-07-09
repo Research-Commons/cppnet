9fa5959804e10ee841bd10723a2aa58883679a2c
Aditya Singh <adityasingh@researchcommons.ai>
2025-07-09 12:51:57 +0000

    Added callbacks.h code

---
+++ include/http/parser/callbacks.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include "../request.h"
+#include "utils.h"
+
+namespace http
+{
+
+    // Forward declaration of the parser class (to be implemented later)
+    class Parser;
+
+    // Callback functions for HTTP parsing events
+    namespace callbacks
+    {
+
+        // Called when the HTTP method is parsed
+        int on_method(Parser &parser, const std::string &method_str);
+
+        // Called when URL data is parsed (may be called multiple times)
+        int on_url(Parser &parser, const char *at, size_t length);
+
+        // Called when a header field (name) is parsed (may be called multiple times)
+        int on_header_field(Parser &parser, const char *at, size_t length);
+
+        // Called when a header value is parsed (may be called multiple times)
+        int on_header_value(Parser &parser, const char *at, size_t length);
+
+        // Called when all headers are complete
+        int on_headers_complete(Parser &parser);
+
+        // Called when body data is parsed
+        int on_body(Parser &parser, const char *at, size_t length);
+
+        // Called when the entire message is complete
+        int on_message_complete(Parser &parser);
+
+    } // namespace callbacks
+
+} // namespace http
